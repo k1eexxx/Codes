ae08a9283d7c47b605356015462bf2e25f45d50d
k1eexxx <109626936+k1eexxx@users.noreply.github.com>
2026-04-08 15:46:45 +0300

    Задача2.cpp

--- Задача2.cpp
+++ Задача2.cpp
@@ -5,18 +5,17 @@ using namespace std;
 int main() 
 {
     int m;
-    
     cout << "Введите номер масти (1-4): ";
     cin >> m;
-    
+
     switch (m)
     {
-        case 1: cout << "пики" << endl; break;
-        case 2: cout << "трефы" << endl; break;
-        case 3: cout << "бубны" << endl; break;
-        case 4: cout << "червы" << endl; break;
-        default: cout << "Ошибка: номер масти должен быть от 1 до 4" << endl;
+        case 1: cout << "пики"; break;
+        case 2: cout << "трефы"; break;
+        case 3: cout << "бубны"; break;
+        case 4: cout << "червы"; break;
+        default: cout << "Ошибка";
     }
-    
+
     return 0;
 }
